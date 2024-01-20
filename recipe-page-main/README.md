d1b6c9a170142765770e798123ca8cc4e0f756de
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-20 18:45:39 -0300

    recipe front step 1

---
+++ recipe-page-main/README.md
@@ -0,0 +1,7 @@
+# Recipe page
+
+![Design preview for the Recipe page coding challenge](./design/desktop-design.jpg)
+
+Tailwindcss
+
+
---
+++ recipe-page-main/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+        'young': ['"Young Serif"', 'serif']
+      },
+      colors: {
+        "nutmeg": "hsl(14, 45%, 36%)",
+        "dark-raspberry": "hsl(332, 51%, 32%)",
+        "rose-white": "hsl(330, 100%, 98%)",
+        "eggshell": "hsl(30, 54%, 90%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+};
+
