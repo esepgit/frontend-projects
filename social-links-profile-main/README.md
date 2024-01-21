3b437b3452f2feb06f75f026393f86a6baa6a7d9
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-21 19:17:31 -0300

    03 - social link profile ready

---
+++ social-links-profile-main/README.md
@@ -0,0 +1,9 @@
+# Social links profile
+
+[social links preview](./assets/preview.gif)
+
+## Technologies
+
+- HTML
+- CSS
+- Tailwindcss
---
+++ social-links-profile-main/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "hsl(75, 94%, 57%)",
+        grey: "hsl(0, 0%, 20%)",
+        "dark-grey": "hsl(0, 0%, 12%)",
+        "off-black": "hsl(0, 0%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
+
