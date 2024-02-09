d4928af1a2e22e7a1366788757ebb1a8fbc287cb
esepgit <150302936+esepgit@users.noreply.github.com>
2024-02-09 19:05:24 -0300

    infinite scroll

--- poke-esep/src/components/Pokemons.jsx
+++ poke-esep/src/components/Pokemons.jsx
@@ -1,27 +1,56 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from "axios"
 import PokemonList from "./PokemonList"
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver"
+
+const INITIAL_LIMIT = 40
+const INCREASE_LIMIT = 20
 
 function Pokemons() {
   const [allPokemons, setAllPokemons] = useState([])
+  const [pokemonName, setPokemonName] = useState("")
+  const [limit, setLimit] = useState(INITIAL_LIMIT)
 
+  const targetObserver = useRef(null)
+  const entry = useIntersectionObserver(targetObserver, {})
+  const isVisible = !!entry?.isIntersecting
+
+  const pokemonsByName = allPokemons.filter(pokemon => pokemon.name.includes(pokemonName))
+  
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=898")
       .then(({ data }) => setAllPokemons(data.results))
       .catch((err) => console.log(err))
   }, [])
+
+  useEffect(() => {
+    if (isVisible) {
+      const maxPokemons = pokemonsByName.length
+      const newLimit = limit + INCREASE_LIMIT
+      newLimit > maxPokemons ? setLimit(maxPokemons) : setLimit(newLimit)
+    } 
+  }, [isVisible])
+
+  useEffect(() => {
+    setLimit(INITIAL_LIMIT)
+  }, [pokemonName])
+
   return (
     <section className="p-4 py-5 ">
       <form>
         <div className="bg-white p-2 rounded-2xl text-lg text-center">
           <input
+            value={pokemonName}
+            onChange={(e) => setPokemonName(e.target.value.toLowerCase())}
             className="outline-none"
             type="text"
             placeholder="Search your Pokemon"
           />
         </div>
       </form>
-      <PokemonList pokemons={allPokemons} />
+      <PokemonList pokemons={pokemonsByName.slice(0, limit)} />
+      {/*target observer*/}
+      <span ref={targetObserver}></span>
     </section>
   );
 }
